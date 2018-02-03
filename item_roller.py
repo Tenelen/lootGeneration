966b8ecb89608d98869e9f1c749f466a64c3b370
Tenelen <tfptenelen@gmail.com>
2018-02-02 23:47:07 -0700

    just a simple file for rolling items outside of the class

---
+++ item_roller.py
@@ -0,0 +1,11 @@
+from lootGenerator import LootGeneration
+
+star_destroyer = LootGeneration('star destroyer')
+
+star_destroyer.create_item()
+star_destroyer.create_item()
+star_destroyer.create_item()
+star_destroyer.create_item()
+star_destroyer.create_item()
+star_destroyer.create_item()
+star_destroyer.create_item()
