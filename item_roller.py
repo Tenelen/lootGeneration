1e7aab6989b8d0fd96cd9dde3ea7ee64b014cd7c
Tenelen <tfptenelen@gmail.com>
2018-02-05 19:38:04 -0700

    Changes to printed format of weapons, and fixes to rarity selection. Saving changes before re-writing the entire mod_pool section.

--- item_roller.py
+++ item_roller.py
@@ -1,11 +1,37 @@
 from lootGenerator import LootGeneration
 
-star_destroyer = LootGeneration('star destroyer')
+star_destroyer = LootGeneration('star destroyer', givenRarity='legendary')
+rusted_dagger = LootGeneration('rusted dagger', givenRarity='legendary')
 
-star_destroyer.create_item()
-star_destroyer.create_item()
-star_destroyer.create_item()
-star_destroyer.create_item()
-star_destroyer.create_item()
-star_destroyer.create_item()
-star_destroyer.create_item()
+equipped_items = []
+
+rusted_dagger.equip_item()
+
+
+if rusted_dagger.equipped == True:
+    equipped_items.append(rusted_dagger)
+
+print(equipped_items)
+
+rusted_dagger.create_item()
+#print(rusted_dagger.name)
+#print(rusted_dagger.createdItemData)
+
+
+damage = 0
+"""print(damage)
+
+if rusted_dagger.equipped == True:
+    for key in rusted_dagger.createdItemData:
+        if key == "physical damage":
+            damage += rusted_dagger.createdItemData[key]
+        elif key == "cold damage":
+            damage += rusted_dagger.createdItemData[key]
+        elif key == "lightning damage":
+            damage += rusted_dagger.createdItemData[key]
+        elif key == "fire damage":
+            damage += rusted_dagger.createdItemData[key]
+else:
+    print("This item is not equipped.")
+print(damage)
+"""
--- lootGenerator.py
+++ lootGenerator.py
@@ -13,9 +13,14 @@ import random
 #Start with a simple idea: Roll for number of mods (1-6)
 
 #For now we're going to make all item types the same generator
+
+
+
 class LootGeneration():
-    def __init__(self, name, rarity='normal'):
-        self.rarity = rarity
+    def __init__(self, name, givenRarity='default'):
+        self.equipped = False
+        self.givenRarity = givenRarity
+        self.realRarity = ''
         self.name = name
         self.baseMods = 1
         self.maxMods = 1
@@ -39,27 +44,31 @@ class LootGeneration():
 
         #Rolls the number of modifiers that the loot will have (1-6)
     def roll_mods(self):
-        self.rarity = (random.sample(self.rarityChart, 1))[0]
-        if self.rarity == 'normal':
+        if self.givenRarity == 'default':
+            self.realRarity = (random.sample(self.rarityChart, 1))[0]
+        else:
+            self.realRarity = self.givenRarity
+
+        if self.realRarity == 'normal':
+            self.baseMods = 1
             self.maxMods = 1
-        elif self.rarity == 'magic':
+        elif self.realRarity == 'magic':
+            self.baseMods = 1
             self.maxMods = 2
-        elif self.rarity == 'rare':
+        elif self.realRarity == 'rare':
             self.baseMods = 2
             self.maxMods = 4
-        elif self.rarity == 'legendary':
+        elif self.realRarity == 'legendary':
             self.baseMods = 4
             self.maxMods = 6
         self.numOfMods = random.randint(self.baseMods, self.maxMods)
-        print ('\n' + self.name.title())
-        """print (str(self.numOfMods) + " Mod(s)")"""
 
         #Picks the mods from the mod_pool based on the number of mods found above
         #puts them into a list based on the Key from Mod_Pool
         #
     def pick_mods(self):
-        self.pickedMods = []
         self.potentialMods = []
+        self.pickedMods = []
         #Takes the keys from the Mod_pool dictionary and turns them into a list of
         #potential mods
         for key in self.mod_pool:
@@ -73,11 +82,38 @@ class LootGeneration():
         #the values established in the mod_pool and
         #sorts the mods into a format that's better for the player
     def sort_mods(self):
-        for pickedMod in self.pickedMods:
-            formatted_mod = "+" + str(self.mod_pool[pickedMod]) + " To " + str(pickedMod).title()
-            print(formatted_mod)
+        self.createdItemData = {}
+        self.formatted_mods = ''
+        #takes items from the pickedMods and adds them to a dictionary of
+        #the item data based on the key pMod and the value that relates from mod_pool
+        for pMod in self.pickedMods:
+            self.createdItemData.update({pMod:self.mod_pool[pMod]})
+        #formats the mod into a "+ NUM to STAT format"
+        for mod in self.createdItemData:
+            self.formatted_mods += "+" + str(self.createdItemData[mod]) + " To " + str(mod).title() + '\n'
+
+        #global_mods = self.createdItemData
+
+    def print_item_entirity(self):
+        #Prints a complete format for the item (name, num of mods, rarity, mods )
+        #Optional debug for createdItemData dictionary to print here as well
+        print ('\n' + self.name.title())
+        print("Number of Mods: " + str(self.numOfMods))
+        print(self.realRarity.title())
+        #print(self.createdItemData)
+        print(self.formatted_mods)
+        print("\n")
+
+    def equip_item(self):
+        #checks in the item is equipped, for future purposes
+        self.equipped = True
+        print(self.equipped)
+
+    #def add_itemStats_to_player(self)
 
     def create_item(self):
         self.roll_mods()
         self.pick_mods()
         self.sort_mods()
+
+        #self.print_item_entirity()
